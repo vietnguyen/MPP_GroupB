c93e36dc750a48db20e27d2f9f914bd9c26fd91c
Chinonso Ikewelugo <chinonso.ikewelugo@pwcng-rpa.com>
2024-12-03 21:04:23 -0600

    add prob 1 answer

---
+++ src/Lab5/prob1/MallardDuck.java
@@ -0,0 +1,23 @@
+package Lab5.prob1;
+
+public class MallardDuck extends Duck{
+    @Override
+    public void display() {
+        System.out.println("Displaying");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("swimming");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("fly with wings");
+    }
+
+    @Override
+    public void quack() {
+        System.out.println("quacking");
+    }
+}
