6026ffa1b44ff009bebb3cb25b02188a6f8d2583
Viet Nguyen <viet@dhis2.org>
2024-12-13 19:29:31 -0600

    feat: add solution for 4,5,6

--- src/Lab9/lesson9/labs/prob6/Section.java
+++ src/Lab9/lesson9/labs/prob6/Section.java
@@ -6,13 +6,22 @@ import java.util.stream.Stream;
 
 public class Section {
 	public static Stream<String> streamSection(Stream<String> stream, int m, int n) {
-		return null; //implement 
+		int limit = (n + 1) - m;
+		return stream.skip(m).limit(limit);
 	}
-	
+
+	/**
+	 * Expected output
+	 * aaa, bbb, ccc, ddd
+	 * ccc, ddd, eee, fff
+	 * hhh, iii
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println(streamSection(nextStream(), 0, 3).collect(Collectors.joining(", ")));
 		System.out.println(streamSection(nextStream(), 2, 5).collect(Collectors.joining(", ")));
-		System.out.println(streamSection(nextStream(), 7, 8).collect(Collectors.joining(", ")));	
+		System.out.println(streamSection(nextStream(), 7, 8).collect(Collectors.joining(", ")));
+		
 	}
 	
 	//support method for the main method -- for testing
